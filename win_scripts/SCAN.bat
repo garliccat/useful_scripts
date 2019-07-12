84c3a84fcb2b6565a7310d114eac4ea807bf0303
Anton Petkov <balabunat@gmail.com>
2019-07-12 10:53:42 +0300

    added cyrillic support

--- win_scripts/SCAN.bat
+++ win_scripts/SCAN.bat
@@ -1,12 +1,14 @@
 REM ### this script is for auto scanning using NAPS2 , that obviously must be installed beforehand.
 REM ### please check the NAPS2 location according to your system and scanning target place also
 
+chcp 1251 >nul
+
 @echo off
-cd C:\Program Files\NAPS2\
+cd C:\Program Files (x86)\NAPS2\
 
 :START
 ECHO SCANNING...
-naps2.console -o "C:\Users\Anton\Documents\Scanned Documents\$(YYYY)-$(MM)-$(DD)_$(hh)-$(mm)-$(ss).jpg" --jpegquality 90
+naps2.console -o "C:\Users\Anton\Google Диск\scans\$(YYYY)-$(MM)-$(DD)_$(hh)-$(mm)-$(ss).pdf"
 
 set /p id="Make another scan? (y/n): "
 
