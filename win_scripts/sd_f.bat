7e23a16748393506190ae61b37cc20f1ac0c3b9f
Anton Petkov <balabunat@gmail.com>
2023-08-21 21:24:41 +0300

    Delete sd_f.bat

--- win_scripts/sd_f.bat
+++
@@ -1,5 +0,0 @@
-REM ### sd_f is for Shut Down Fast
-REM ### hang it to keyboard shortcut and turn off PC like a boss, nice, easy and fast.
-REM ## not a script at all, but very handy
-
-shutdown /s /f /t 0
