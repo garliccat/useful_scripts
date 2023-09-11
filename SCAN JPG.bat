21fb80d6071aad99e400b0e37ca43ca9f69eba07
Anton Petkov <balabunat@gmail.com>
2023-09-11 10:27:50 +0300

    windows encoding added fro cyrilic symbols in path

---
+++ SCAN JPG.bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 1251 >nul
+cd C:\Program Files (x86)\NAPS2\
+
+:START
+SET id=""
+ECHO SCANNING...
+naps2.console -o "C:\Users\anton\Мой диск\scans\$(YYYY)$(MM)$(DD)$(hh)$(mm)$(ss).jpg"
+ECHO[
+ECHO DONE!
+ECHO[
+set /p id="Make another scan? ('y' to proceed or any key to exit): "
+
+if "%id%" == "y" GOTO START
